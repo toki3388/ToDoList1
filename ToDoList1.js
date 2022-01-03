384cfdff9835dbe98f85fd1690e12d0a66ecacb7
toki3388 <ksgrat678@gmail.com>
2022-01-03 20:43:21 +0900

    字数制限を追加

--- ToDoList1.js
+++ ToDoList1.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const todo = document.getElementById('todo'); //タスク
-const deadline = document.querySelector('input[type="date"]'); //日付
+const todo = document.getElementById('todo');   //タスク
+const deadline = document.querySelector('input[type="date"]');  //日付
 const add = document.getElementById('add'); //追加ボタン
 const table = document.getElementById('table'); //表
 
@@ -15,9 +15,12 @@ add.addEventListener('click', function(){
     deadline.value = ''; */
 
     if(todo.value != '' && deadline.value != ''){
-　　    const tr = document.createElement('tr'); 
+        item.todo = todo.value;
+        item.deadline = deadline.value; 
 
-　　    for (const prop in item){                     //propはプロパティ名
+        const tr = document.createElement('tr'); 
+
+        for (const prop in item){                     //propはプロパティ名
             const td = document.createElement('td');
             td.textContent = item[prop]; 
             tr.appendChild(td); 
