80ea579dba989b0c9faed9de5342d0663253f8f8
toki3388 <ksgrat678@gmail.com>
2022-01-25 20:29:38 +0900

    削除ボタンの追加

--- ToDoList1.js
+++ ToDoList1.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const todo = document.getElementById('todo');   //タスク
 const deadline = document.querySelector('input[type="date"]');  //日付
 const add = document.getElementById('add'); //追加ボタン
@@ -11,26 +9,35 @@ add.addEventListener('click', () => {
     item.todo = todo.value; 
     item.deadline = deadline.value; 
 
-    /*todo.value = '';
-    deadline.value = ''; */
-
     if(todo.value != '' && deadline.value != ''){
         item.todo = todo.value;
         item.deadline = deadline.value; 
 
         const tr = document.createElement('tr'); 
+        const remove = document.createElement('button');
+        remove.id = 'delete'; //removeのid名を作成
 
         for (const prop in item){                     //propはプロパティ名
             const td = document.createElement('td');
             td.textContent = item[prop]; 
-            tr.appendChild(td); 
+            remove.textContent = '削除';
+            tr.appendChild(td);
+            tr.appendChild(remove); 
         }
 
-        table.append(tr); 
+        table.append(tr);
+
+        tr.appendChild(remove);
+
+        remove.addEventListener('click', () => {
+            removeTask(remove);
+        });
+
+        const removeTask = (remove) => {
+            const chosenTask = remove.closest('tr');
+            table.removeChild(chosenTask);
+        };
     }else{
         window.alert('タスクと期日を入力してください')
-        return;
     }
-    
 });
-
