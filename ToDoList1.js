b35a34ce2ee28c438a032b1f96d8cc097cbeb12a
toki3388 <ksgrat678@gmail.com>
2022-02-22 00:28:25 +0900

    ローカルストレージの機能を追加

--- ToDoList1.js
+++ ToDoList1.js
@@ -2,6 +2,40 @@ const todo = document.getElementById('todo');   //タスク
 const submitTime = document.getElementById('submitDate');  //日付
 const add = document.getElementById('add'); //追加ボタン
 const taskList = document.getElementById('taskList');
+//const favorite = document.getElementById('favorite');
+
+let list = [];
+const storage = localStorage;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const json = storage.TodoList;
+  if (json == undefined) {
+    return;
+  }
+  list = JSON.parse(json);
+  for (const item of list) {
+    addItem(item);
+  }
+});
+
+const addItem = (item) => {
+
+    const remove = document.createElement('button');
+    remove.id = 'delete'; //removeのid名を作成
+
+    for (const prop in item){                     //propはプロパティ名
+        const label = document.createElement('label');
+        label.id = 'labelElement';
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = 'checkBox';
+        label.textContent = item[prop]; 
+        remove.textContent = '削除';
+        label.appendChild(input);
+        label.appendChild(remove);
+        taskList.appendChild(label);
+    }
+};
 
 add.addEventListener('click', () => {
     const item = {};
@@ -11,9 +45,16 @@ add.addEventListener('click', () => {
         todo.value = ''; //タスクを追加すると同時にラベル内を空にする
 
         const remove = document.createElement('button');
-        remove.id = 'delete'; //removeのid名を作成
+        remove.id = 'delete';
 
-        for (const prop in item){                     //propはプロパティ名
+        addItem(item);
+
+        list.push(item);
+        storage.TodoList = JSON.stringify(list);
+
+        
+
+        /*for (const prop in item){                     //propはプロパティ名
             const label = document.createElement('label');
             label.id = 'labelElement';
             const input = document.createElement('input');
@@ -24,16 +65,29 @@ add.addEventListener('click', () => {
             label.appendChild(input);
             label.appendChild(remove);
             taskList.appendChild(label);
-        }
+        }*?
+
+        /*$(function () {
+            $('#checkBox').on('change', function(){
+                window.alert('お気に入りに追加されました');
+                relocateTask();
+            });
+        });
+
+        const relocateTask = (changeLabel) => {
+            const selectTask = changeLabel.closest('label');
+            favorite.appendChild(selectTask);
+        };*/
 
         remove.addEventListener('click', () => {
             removeTask(remove);
         });
 
-        const removeTask = (remove) => {
-            const chosenTask = remove.closest('label');
+        const removeTask = (removal) => {
+            const chosenTask = removal.closest('label');
             taskList.removeChild(chosenTask);
         };
+
     }else{
         window.alert('タスクを入力してください');
     }
